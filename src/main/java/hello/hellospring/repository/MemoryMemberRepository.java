e098f218d8af9b5a26f690a6f2bb883c843f71ab
danggeuni <dydrnxkeh@naver.com>
2024-01-08 18:51:54 +0900

    controller di

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,12 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+
+@Repository
 public class MemoryMemberRepository implements MemberRepository {
 //    값 저장할 map 컬렉션
     private static Map<Long, Member> store = new HashMap<>();
